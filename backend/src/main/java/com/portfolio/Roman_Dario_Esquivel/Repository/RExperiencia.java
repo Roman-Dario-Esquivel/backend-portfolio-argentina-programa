4dd797675fffbe2a052933c220bc949d92b47e3b
Roman-Dario-Esquivel <Roman-Dario-Esquivel>
2022-10-31 22:40:34 -0300

    login andando ultima por ahora

--- backend/src/main/java/com/portfolio/Roman_Dario_Esquivel/Repository/RExperiencia.java
+++ backend/src/main/java/com/portfolio/Roman_Dario_Esquivel/Repository/RExperiencia.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.portfolio.Roman_Dario_Esquivel.Repository;
 
-/**
- *
- * @author esqui
- */
-public interface RExperiencia {
-    
+import com.portfolio.Roman_Dario_Esquivel.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
 }
+
