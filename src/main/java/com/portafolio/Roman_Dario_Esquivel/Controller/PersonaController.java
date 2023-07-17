ab29836769ad0bde4ff56246ce6317e534dbba01
Roman-Dario-Esquivel <Roman-Dario-Esquivel>
2023-07-17 14:04:07 -0300

    ultima version hosting firebase prueba producion

--- src/main/java/com/portafolio/Roman_Dario_Esquivel/Controller/PersonaController.java
+++ src/main/java/com/portafolio/Roman_Dario_Esquivel/Controller/PersonaController.java
@@ -22,8 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = {"https://romandarioesquivel-9162f.web.app/","https://romandarioesquivel-9162f.firebaseapp.com/"})
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://romandarioesquivel-9162f.web.app/","https://romandarioesquivel-9162f.firebaseapp.com/"})
 public class PersonaController {
      @Autowired
     ImpPersonaService personaService;
